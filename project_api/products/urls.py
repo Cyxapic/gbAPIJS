487efce4542d6c42b198f13c228d57a1c0943f22
Cyxapic <truecyxapic@yandex.ru>
2019-02-23 13:46:46 +0300

    Change all Router from Defaul to Simple

--- project_api/products/urls.py
+++ project_api/products/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from . import views
 
 
-products_router = routers.DefaultRouter()
+products_router = routers.SimpleRouter()
 products_router.register(r'categories', views.CategoryViewSet)
 products_router.register(r'products', views.ProductViewSet)
--- project_api/shop/urls.py
+++ project_api/shop/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from . import views
 
 
-shop_router = routers.DefaultRouter()
+shop_router = routers.SimpleRouter()
 shop_router.register(r'description', views.ShopDescriptionViewSet)
 shop_router.register(r'payment-methods', views.PaymentMethodsViewSet)
